e6d73cc703738470696927015f157657e114d7d0
Muna Mohamed <92002717+munamoha@users.noreply.github.com>
2021-12-04 23:44:41 -0500

    Add files via upload

--- finalProjectClient.js
+++ finalProjectClient.js
@@ -8,43 +8,67 @@ make buttons in homepage work,
 var url = "http://localhost:3001/post"
 
 var columns = 6; 
-var myName;
+var rows = 2;
+var myName="test";
 var clickable=false;
 var foundCards=[];
 var card1, card2;
-/*var rows = 4;*/
+var difficulty='easy'
+var misses=0;
+var playerData;
+var leaderboard;
 
+window.onload = function()
+{
+    x=window.location.href
+    
+    
+    if (x.slice(-13,)=='gamePage.html'){
+        var playerValues = JSON.parse(sessionStorage.player); //player values from homePage
+        difficulty = playerValues['difficulty'];
+        myName = playerValues['name'];
+        setRows();
+        preperation(); //draw the game board
+        //creates the deck, rows*6 is the n of cards in the deck
+        deck(rows*6);
+        //starts the functions to show cards
+        $.post(
+        url+'?data='+JSON.stringify({
+        'action':'show', 
+        }),
+        response
+    );   }
+    else if((x.slice(-16,)=='leaderBoard.html')){
+        var leaderboardValues = JSON.parse(sessionStorage.leaderboard); //player values from homePage
+        
+        displayLeaderboard(leaderboardValues['leaderboard'],leaderboardValues['playerData'])
+
+    }
+}
 function updateDifficulty(d) {
-    window.location.assign("gamePage.html"); 
-    window.rows = d;
-    startGame();
+    difficulty=d
+    startGame(difficulty);
 }
-/*
-function difficultyChange() {
-$('#easy').click(updateDifficulty(2)
-);
+function startGame(d){
+    var Name = prompt('Enter your name here:')
+    sessionStorage.player = JSON.stringify({
+        'name':Name,'difficulty':d 
+        });
+    window.location.href="./gamePage.html";
 
-$('#medium').click(updateDifficulty(3));
 
-$('#hard').click(updateDifficulty(4));
-}*/
-
-function startGame(){
-    preperation(); //draw the game board
-    myName = prompt("Please enter your name", "");
-    deck(rows*6);
 }
+
 /* 
  * Create the game board, includes 
  * x number of rows(depending on difficulty)
  * 6 columns
  * img of cards
  */
-
 function preperation(){
     
     //add rows
-    for (let i = rows; i>0; i--){
+    for (var i = rows;i>0;i--){
         //for each row create a div
         var newDiv = document.createElement("div");
 		// set its id and class
@@ -80,6 +104,7 @@ function deck(numberOfCards){
         url+'?data='+JSON.stringify({
         'action':'createDeck', 
         'numberOfCards':numberOfCards,
+        'difficulty':difficulty
         }),
         response
     );
@@ -87,22 +112,22 @@ function deck(numberOfCards){
 
 }
 function response(data){
-    var response = JSON.parse(data);
-    
-    console.log(data);
-    if (response['action']=='createDeck'){attemptOnOff();}
-    
-    
-    else if(response['action']=='selectCard'){
-        cardId=response['cardId'];
-        image=response['image'];
-        match=response['match'];
-        win=response['win'];
+
+    var res = JSON.parse(data);
+
+    if (res['action']=='createDeck'){attemptOnOff();
+    }
+    else if(res['action']=='selectCard'){
+        cardId=res['cardId'];
+        image=res['image'];
+        match=res['match'];
+        misses=res['misses']
+        console.log(misses)
+        
         $('#identifier'+cardId).attr("src",image);
         
-        console.log(win)
-
         
+    
         if (match==3){//if there wasnt a need for a match
             card1=cardId
 
@@ -110,17 +135,49 @@ function response(data){
         else if(match==2){//if they dont match
             card2=cardId
             cardList=[card1,card2]
-            cover(cardList, 1000)//
+            cover(cardList, 1000)//W
         }
-        else if(win){
-        alert("congrats! you won!")}
+        
         attemptOnOff();
     }
+    else if(res['action']=='show'){
+        
+        show(res['array'])
+        
+    }
+    else if(res['action']=='playerData'){
+        cardId=res['cardId'];
+        image=res['image'];
+        
+        $('#identifier'+cardId).attr("src",image);
+        $.post(
+            url+'?data='+JSON.stringify({
+            'action':'win', 
+            'name':myName,
+            'difficulty':difficulty,
+            'misses':misses
+            }),
+            response
+        );
+        
+        
+    }
+    else if(res['action']=='win'){
+
+        sessionStorage.leaderboard = JSON.stringify({
+            'leaderboard':res['leaderboard'],'playerData':res['player'] 
+            });
+        
+        
+        alert("you won! Click okay to move to the leaderboard")
+        window.location.assign("leaderBoard.html")
+
+      
+    }
+
 }
-
-
 function selectCard(event){
-    console.log(event.data.id)
+    
     attemptOnOff();
     $.post(
         url+'?data='+JSON.stringify({
@@ -133,9 +190,9 @@ function selectCard(event){
 }
 /*allows  the user to click the cards, only activates when server has returned all requests */
 function attemptOnOff(){
-    console.log(clickable)
+    
     if (clickable){
-        console.log("cant click")
+        
         $(".imgColumn").off("click");
         clickable=false
     }
@@ -145,7 +202,7 @@ function attemptOnOff(){
                 $('#identifier'+((i-1)*6 + j)).click({id: (i-1)*6+j},selectCard);
         
                 
-    }}clickable=true;console.log("can click")}
+    }}clickable=true;}
 
 }
 /*flips the cards after a set amount of time */
@@ -153,6 +210,53 @@ function cover(ids,time){
   
     setTimeout(function(){for(i=0;i< ids.length;i++){
         $('#identifier'+ids[i]).attr('src','./cards/cover.png');
-        console.log('covering')
+        
     }},time)
 }
+function show(array){
+    ids=[]
+    
+    console.log(array)
+    for (i=0;i<array.length;i++){
+        $('#identifier'+(i+1)).attr('src',array[i])
+        //this sets up id's for the cover function
+        ids.push(i+1)
+    }
+    console.log(ids)
+    cover(ids,4000)
+}
+function displayLeaderboard(leaderboard,playerData){
+    console.log("i am working")
+    console.log(leaderboard)
+    $('#leaderboardTable').append( '<tr><td>Name</td><td>Score</td></tr>' );
+
+   
+    for(i=0;i<leaderboard.length&&i<10;i++){
+        $('#leaderboardTable').append( '<tr><td>'+leaderboard[i]['name']+'</td><td>'+leaderboard[i]['misses']+'</td></tr>' )
+    }
+    $('#leaderboardTable').append( '<tr id = "results"><td>Your results here:</td></tr>' );
+    console.log(playerData)
+    $('#leaderboardTable').append( '<tr><td>'+playerData['name']+'</td><td>'+playerData['misses']+'</td></tr>' );
+
+
+}
+function setRows(){
+    switch (difficulty) {
+        case 'easy':
+            rows=2;
+            break;
+        case 'medium':
+            rows=3;
+            break;
+        case 'hard':
+                rows=4;
+            break;
+    }
+
+}
+function backHome(){
+window.location.assign("homePage.html")}
+/*
+ *known issues:
+ player cant select cards when the cards are being shown 
+ */
--- finalProjectServer.js
+++ finalProjectServer.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var app = express();
+//set required node.js stuff
+const express = require('express');
+const app = express();
+const fs = require('fs');
+//leaderboard file stuff
+var rawdata=fs.readFileSync('./leaderboard.json');
+var leaderboard = JSON.parse(rawdata);
 
-var order;
-var counter=0;
-var card1, card2,match;
+//set all the variables
+var counter=0, currentMatches=0, misses=0;
+var card1, card2, match, win, order;
 var matchesNeeded=6;//amount of matches needed to win, changes by difficulty
-var currentMatches=0;
-var win;
+
+
+
 app.post('/post', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     console.log("New express client");
@@ -16,7 +22,10 @@ app.post('/post', (req, res) => {
     
     
     if (z['action']=='createDeck'){
-   
+
+        refresh()//refreshes all values that need to be rest
+        //prepares the deck, and preps for the difficulty
+        setMatches(z['difficulty'])
         order=prepareDeck(z['numberOfCards']);
         var jsontext =JSON.stringify({
             'action':'createDeck',
@@ -26,6 +35,7 @@ app.post('/post', (req, res) => {
         res.send(jsontext)
     }
     else if(z['action']=='selectCard'){    
+      
         counter++
         img=order[parseInt((z['cardId'])-1)];
         /*if this is the second card the user clicks, 
@@ -35,29 +45,72 @@ app.post('/post', (req, res) => {
         if (counter==2){
             card2=img;
             counter=0;
+            if(duplicateCheck!=z['cardId']){
             returns=evaluate(card1, card2)
             match=returns[0]
             win=returns[1]
+            }else{
+                misses++
+                match=2;
+            }
             
         }
         //if this is the first card the user clicks
         else{
+            duplicateCheck=z['cardId']
             card1=img;
             match=3
         }
         
         console.log(win)
-        var jsontext =JSON.stringify({
-            'action':'selectCard',
-            'image': img,
-            'cardId':z['cardId'],
-            'match':match,
-            'win':win
-        });
+        if(win)
+            var jsontext =JSON.stringify({
+                //since it wins, it calls for playerData for leaderboard
+                'action':'playerData',
+                'image': img,
+                'cardId':z['cardId'],
+            });
+        else{
+            var jsontext =JSON.stringify({
+                'action':'selectCard',
+                'image': img,
+                'cardId':z['cardId'],
+                'match':match,
+                'misses':misses
+        });}
         res.send(jsontext)
     
     
         }
+    else if(z['action']=='show'){
+        var jsontext =JSON.stringify({
+            'action':'show',
+            'array':order
+        });
+        res.send(jsontext)
+    }
+    else if(z['action']=='win'){//when win add the stuff to leaderboard stuff
+        playerData={'name':z['name'],'misses':z['misses'],'difficulty':z['difficulty']}
+
+        //inputs the player's info into the leaderboard
+        leaderboard.push(playerData)
+
+        //updates the leaderboard
+        var data = JSON.stringify(leaderboard);
+        fs.writeFileSync('leaderboard.json', data);
+
+        //prepares the displayed leaderboard
+        display=prepLeaderboard(playerData)
+        console.log(display)
+        var jsontext =JSON.stringify({
+            'action':'win',
+            'leaderboard': display,
+            'player':playerData,
+
+    });
+    res.send(jsontext)
+
+    }
     
 
 
@@ -126,30 +179,96 @@ function doubleArray(array){
 *return match to the function
 *if there are enough matches, go to win
 */
-function evaluate(card1, card2){
-  
+function evaluate(card1, card2,duplicateCheck){
+    
     if(card2==card1){
-        match=1
-        currentMatches++
+        match=1;
+        currentMatches++;
         if (currentMatches==matchesNeeded){
-            win=true
+            win=true;
         }
-    }else{match=2}
+    }else{
+        misses++
+        match=2}
 return [match, win];
 
 }
+/*function refreshes all values that need to be refreshed whenever the page gets loaded/ a new deck gets created
+*/
+function refresh(){
+    match=null,card1=null,card2=null,win=null;
+    currentMatches=0;
+    misses=0;
+}
+/*
+*prepares the leaderboard
+*sorts the leaderboard
+*adds the players result
+ */
+function prepLeaderboard(playerData){
+    
+    leaderboardToDisplay=sortleaderboard(leaderboard,playerData['difficulty']);
+    
+    return(leaderboardToDisplay)
+    
+
+}
+function sortleaderboard(array,difficulty){
+    var sortedList=[];//original list, keeps the order of the dictionary
+    var arrayByDifficulty=[];
+
+    for(i=0;i<array.length;i++){
+        if(array[i]['difficulty']==difficulty){
+        arrayByDifficulty.push(array[i]);
+        }
+    }console.log(arrayByDifficulty)
+
+    var sortedArray=[]//the sorted array
+    for(i=0;i<arrayByDifficulty.length;i++){
+        sortedList.push(arrayByDifficulty[i]['misses'])
+    }
+  
+    sortedList.sort(function(a, b){return a - b});
+    console.log(sortedList+'list')
+    for(i=0;i<sortedList.length;i++){
+        for(j=0;j<sortedList.length;j++){
+            if(sortedList[i]==arrayByDifficulty[j]['misses']){
+                sortedArray.push(arrayByDifficulty[j])
+                //this prevents duplicates
+                arrayByDifficulty[j]='filler'
+            }
+        } 
+    }
+   return sortedArray
+}
+function setMatches(difficulty){
+    switch(difficulty){
+        case 'easy':
+            matchesNeeded=6;
+            break;
+        case 'medium':
+            matchesNeeded=9;
+            break;
+        case 'hard':
+            matchesNeeded=12;
+            break;
+    }
+
+    }
+
+
 
 
 /*stuff to work on
 in general
 the last card is not shown
 no point system yet
-server doesnt reset when you load the page(not all the values reset)
-
 in client: 
 nitpicky because of the timeout function, it is possible to click on a card while the two other cards are still up(this might not be a large issue)
 
 */
+
+
 
 
 
